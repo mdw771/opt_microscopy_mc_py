1c0d7f8d4e399d1ee1f64cd1001128ed5a13fc46
Ming <mingdu2015@u.northwestern.edu>
2016-10-20 20:01:45 -0500

    rm util

--- Photon.py
+++ Photon.py
@@ -1,5 +1,4 @@
 import numpy as np
-from util import *
 
 
 class Photon():
--- util.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-import time
-
-
-def time_rand():
-
-    r = np.random.rand(1, 1)[0, 0]
-    return r
