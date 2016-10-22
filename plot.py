c777969fa99305e4b4894c4b9575dd9ec11dbc8a
Ming <mingdu2015@u.northwestern.edu>
2016-10-22 14:08:22 -0500

    plot_fix

--- plot.py
+++ plot.py
@@ -15,6 +15,7 @@ for n in [1000, 10000, 100000, 1000000]:
     n100 = np.load('phiz_n_{:d}_ri_1.00.npy'.format(n))
     n137 = np.load('phiz_n_{:d}_ri_1.37.npy'.format(n))
     z = out_grids.get_zcoords()
-    plt.plot(z, n100, z, n137)
+    plt.semilogy(z, n100, z, n137)
+    plt.ylim([1e-1, 1e1])
     plt.savefig('comb_n_{:d}'.format(n))
 
